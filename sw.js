d392e705e29665bc52d0c672ed63b3db5f7d2522
rogercastiillo2024-netizen <rogercastiillo2024@gmail.com>
2026-01-29 09:17:08 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,32 @@
+const CACHE_NAME = 'sro-cache-v2';
+
+// Archivos principales que se guardan apenas se instala
+const urlsToCache = [
+  '/',
+  '/index.html',
+  'logo.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      // Si está en cache, lo devuelve. Si no, lo busca en internet
+      return response || fetch(event.request).then(fetchRes => {
+        // Si es una imagen, la guardamos en cache para la próxima vez
+        if (event.request.url.match(/\.(jpg|jpeg|png|gif|svg)$/)) {
+          return caches.open(CACHE_NAME).then(cache => {
+            cache.put(event.request.url, fetchRes.clone());
+            return fetchRes;
+          });
+        }
+        return fetchRes;
+      });
+    })
+  );
+});
