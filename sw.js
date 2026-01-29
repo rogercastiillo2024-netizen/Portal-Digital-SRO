2773118bc29206e2cc92ef64834f3b68da42c7c5
rogercastiillo2024-netizen <rogercastiillo2024@gmail.com>
2026-01-29 10:44:01 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,47 @@
+const CACHE_NAME = 'sro-cache-v3';
+
+// Archivos principales que se guardan apenas se instala
+const urlsToCache = [
+  '/',
+  '/index.html',
+  'logo.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// Este código borra las cachés antiguas automáticamente
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            console.log('Borrando caché antigua:', cache);
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      // Si está en cache, lo devuelve. Si no, lo busca en internet
+      return response || fetch(event.request).then(fetchRes => {
+        // Si es una imagen, la guardamos en cache para la próxima vez
+        if (event.request.url.match(/\.(jpg|jpeg|png|gif|svg)$/)) {
+          return caches.open(CACHE_NAME).then(cache => {
+            cache.put(event.request.url, fetchRes.clone());
+            return fetchRes;
+          });
+        }
+        return fetchRes;
+      });
+    })
+  );
+});
