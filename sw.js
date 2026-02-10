edcfc3860bd063944d25b641f26b0e299af17d67
rogercastiillo2024-netizen <rogercastiillo2024@gmail.com>
2026-02-10 12:00:19 -0400

    Update service worker cache version to v26

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'sro-cache-v25';
+const CACHE_NAME = 'sro-cache-v26';
 
 // Archivos principales que se guardan apenas se instala
 const urlsToCache = [
@@ -53,6 +53,7 @@ self.addEventListener('fetch', event => {
   );
 
 });
+
 
 
 
